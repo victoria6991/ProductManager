2b0058fa7ecb627661c3ac6ddfd8b0e03ddc4460
victoria6991 <miss.victoria-liar@yandex.ru>
2022-02-15 19:26:33 +0300

    new tests added

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -14,8 +14,9 @@ class ProductManagerTest {
     Product secondProduct = new Product(2, "Table", 10_000);
     Book firstBook = new Book(3, "Гарри Поттер и философский камень", 850, "Джоан Роулинг");
     Book secondBook = new Book(4, "Вино из одуванчиков", 550, "Рей Бредберри");
-    Smartphone firstPhone = new Smartphone(5, "13 pro", 140_000, "Apple");
-    Smartphone secondPhone = new Smartphone(6, "galaxy", 50_000, "Samsung");
+    Book thirdBook = new Book(5, "Гарри Поттер и тайная комната", 850, "Джоан Роулинг");
+    Smartphone firstPhone = new Smartphone(6, "13 pro", 140_000, "Apple");
+    Smartphone secondPhone = new Smartphone(7, "galaxy", 50_000, "Samsung");
 
     ProductManager manager = new ProductManager(new ProductRepository());
 
@@ -25,14 +26,22 @@ class ProductManagerTest {
         manager.add(secondProduct);
         manager.add(firstBook);
         manager.add(secondBook);
+        manager.add(thirdBook);
         manager.add(firstPhone);
         manager.add(secondPhone);
     }
 
     @Test
-    void shouldReturnBookByAuthor() {
-        Product[] expected = {firstBook};
+    void shouldReturnTwoProducts() {
+        Product[] expected = {firstBook, thirdBook};
         Product[] actual = manager.searchBy("Джоан");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnBookByAuthor() {
+        Product[] expected = {secondBook};
+        Product[] actual = manager.searchBy("Бредберри");
         assertArrayEquals(expected, actual);
     }
 
@@ -54,6 +63,13 @@ class ProductManagerTest {
     void shouldReturnPhoneByName() {
         Product[] expected = {secondPhone};
         Product[] actual = manager.searchBy("galaxy");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnEmpty() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("книга");
         assertArrayEquals(expected, actual);
     }
 
